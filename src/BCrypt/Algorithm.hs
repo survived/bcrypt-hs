d59aa6220f363740250c0e6f95941a9991e0bea3
Denis Varlakov <dsurv@yandex.ru>
2019-12-18 16:21:56 +0700

    Split large file into several modules

---
+++ src/BCrypt/Algorithm.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module BCrypt.Algorithm
+  ( SymmetricAlgorithm(..)
+  , AlgorithmImplProvider(..)
+  , SymmetricAlgorithmHandler
+  , sAlgHandlerAlg
+  , sAlgHandlerProvider
+  , sAlgHandler
+  , openSymmetricAlgorithm
+  ) where
+
+import Control.Arrow (second)
+import Control.Monad (when)
+import Control.Monad.Trans.Resource (MonadResource, ReleaseKey, allocate)
+
+import Foreign (Ptr, nullPtr)
+import Foreign.C.String (CWString, withCWString)
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Storable (peek)
+
+import qualified BCrypt.Bindings as B
+
+-- | Symmetric Encription Algorithm
+data SymmetricAlgorithm
+  = BCryptAlgRC2
+  | BCryptAlgRC4
+  | BCryptAlgAES
+  | BCryptAlgDES
+  | BCryptAlgDESX
+  | BCryptAlg3DES
+  | BCryptAlg3DES112
+  deriving (Enum, Bounded)
+
+instance Show SymmetricAlgorithm where
+  show = \case
+    BCryptAlgRC2 -> "RC2"
+    BCryptAlgRC4 -> "RC4"
+    BCryptAlgAES -> "AES"
+    BCryptAlgDES -> "DES"
+    BCryptAlgDESX -> "DESX"
+    BCryptAlg3DES -> "3DES"
+    BCryptAlg3DES112 -> "3DES_112"
+
+data AlgorithmImplProvider
+  = MsPrimitiveProvider
+  | MsPlatformCryptoProvider
+  | DefaultProvider
+
+bCryptAlgImplProviderToString :: AlgorithmImplProvider -> Maybe String
+bCryptAlgImplProviderToString = \case
+  MsPrimitiveProvider -> Just "Microsoft Primitive Provider"
+  MsPlatformCryptoProvider -> Just "Microsoft Platform Crypto Provider"
+  DefaultProvider -> Nothing
+
+data SymmetricAlgorithmHandler = SymmetricAlgorithmHandler
+  { sAlgHandlerAlg      :: SymmetricAlgorithm
+  , sAlgHandlerProvider :: AlgorithmImplProvider
+  , sAlgHandler         :: B.BCRYPT_ALG_HANDLE
+  }
+
+openSymmetricAlgorithm :: MonadResource m => SymmetricAlgorithm -> AlgorithmImplProvider -> m (ReleaseKey, SymmetricAlgorithmHandler)
+openSymmetricAlgorithm alg provider =
+  second (SymmetricAlgorithmHandler alg provider) <$> allocate openAlgHandler closeAlgHandler
+  where
+  withCAlg, withCProvider :: (CWString -> IO a) -> IO a
+  withCAlg = withCWString (show alg)
+  withCProvider = case bCryptAlgImplProviderToString provider of
+    Just p -> withCWString p
+    Nothing -> ($ nullPtr)
+
+  openAlgHandler :: IO B.BCRYPT_ALG_HANDLE
+  openAlgHandler =
+    alloca $ \(handler :: Ptr B.BCRYPT_ALG_HANDLE) ->
+    withCAlg $ \c_alg ->
+    withCProvider $ \c_provider -> do
+      status <- B.c_BCryptOpenAlgorithmProvider handler c_alg c_provider 0
+      when (status < 0) $
+        fail "cannot open alg"
+      peek handler
+
+  closeAlgHandler :: B.BCRYPT_ALG_HANDLE -> IO ()
+  closeAlgHandler handler = do
+    status <- B.c_BCryptCloseAlgorithmProvider handler 0
+    when (status < 0) $
+      fail "cannot open alg"
---
+++ src/BCrypt/Properties.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE DefaultSignatures, ScopedTypeVariables, TypeFamilies, FlexibleContexts #-}
+module BCrypt.Properties
+  ( BCryptProperty, PropertyGet, PropertySet
+  , ObjectLengthProp(..)
+  , ChaingModeProp(..), ChainingMode(..)
+  , getAlgorithmProperty
+  , setAlgorithmProperty
+  ) where
+
+import Control.Monad (when)
+import Foreign.C.String (withCWString, withCWStringLen)
+import Foreign.C.Types (CWchar)
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Marshal.Array (allocaArray)
+import Foreign.Ptr (Ptr, castPtr, nullPtr)
+import Foreign.Storable (Storable, sizeOf, peek, poke)
+import System.Win32.Types (PUCHAR, DWORD)
+
+import BCrypt.Algorithm (SymmetricAlgorithmHandler, sAlgHandler)
+
+import qualified BCrypt.Bindings as B
+
+class BCryptProperty p where
+  type PropertyValue p :: *
+  propertyName :: p -> String
+
+class BCryptProperty p => PropertyGet p where
+  marshalBackward :: p -> PUCHAR -> B.ULONG -> IO (PropertyValue p)
+  default marshalBackward :: Storable (PropertyValue p) => p -> PUCHAR -> B.ULONG -> IO (PropertyValue p)
+  marshalBackward _ ptr size = do
+    when (fromIntegral size /= sizeOf (undefined :: PropertyValue p)) $
+      fail "property value has invalid size"
+    peek (castPtr ptr)
+
+class BCryptProperty p => PropertySet p where
+  marshalForward  :: p -> PropertyValue p -> ((PUCHAR, B.ULONG) -> IO a) -> IO a
+  default marshalForward :: Storable (PropertyValue p) => p -> PropertyValue p -> ((PUCHAR, B.ULONG) -> IO a) -> IO a
+  marshalForward _ val f =
+    alloca $ \(ptr :: Ptr (PropertyValue p)) -> do
+      poke ptr val
+      f (castPtr ptr, fromIntegral $ sizeOf val)
+
+-- PROPERTIES
+
+-- | The size, in bytes, of the subobject of a provider
+data ObjectLengthProp = ObjectLengthProp
+
+instance BCryptProperty ObjectLengthProp where
+  type PropertyValue ObjectLengthProp = DWORD
+  propertyName _ = "ObjectLength"
+instance PropertyGet ObjectLengthProp
+
+-- | Chaining mode of the encryption algorithm
+data ChaingModeProp = ChaingModeProp
+
+data ChainingMode
+  = ChainingModeCBC -- ^ Sets the algorithm's chaining mode to cipher block chaining.
+  | ChainingModeCCM -- ^ Sets the algorithm's chaining mode to counter with CBC-MAC mode.
+  | ChainingModeCFB -- ^ Sets the algorithm's chaining mode to cipher feedback.
+  | ChainingModeECB -- ^ Sets the algorithm's chaining mode to electronic codebook.
+  | ChainingModeGCM -- ^ Sets the algorithm's chaining mode to Galois/counter mode (GCM)
+  | ChainingModeNA  -- ^ The algorithm does not support chaining.
+
+instance Show ChainingMode where
+  show = \case
+    ChainingModeCBC -> "ChainingModeCBC"
+    ChainingModeCCM -> "ChainingModeCCM"
+    ChainingModeCFB -> "ChainingModeCFB"
+    ChainingModeECB -> "ChainingModeECB"
+    ChainingModeGCM -> "ChainingModeGCM"
+    ChainingModeNA  -> "ChainingModeN/A"
+
+instance BCryptProperty ChaingModeProp where
+  type PropertyValue ChaingModeProp = ChainingMode
+  propertyName _ = "ChainingMode"
+
+instance PropertySet ChaingModeProp where
+  marshalForward _ value f = withCWStringLen (show value) $ \(ptr, len) ->
+    f (castPtr ptr, fromIntegral len)
+
+getAlgorithmProperty
+  :: (BCryptProperty p, PropertyGet p)
+  => SymmetricAlgorithmHandler -> p -> IO (PropertyValue p)
+getAlgorithmProperty handler prop =
+  withCWString (propertyName prop) $ \propName -> do
+    bufSize <- lookupSize propName
+    allocaArray (fromIntegral bufSize) $ \valueBuf -> do
+      getProp propName valueBuf bufSize
+      marshalBackward prop valueBuf bufSize
+  where
+  lookupSize :: Ptr CWchar -> IO B.ULONG
+  lookupSize propName = alloca $ \(pcbResult :: Ptr B.ULONG) -> do
+    status <- B.c_BCryptGetProperty (sAlgHandler handler) propName nullPtr 0 pcbResult 0
+    when (status < 0) $
+      fail "can't determinate length of property value"
+    peek pcbResult
+  getProp :: Ptr CWchar -> PUCHAR -> B.ULONG -> IO ()
+  getProp propName valueBuf valueBufSize = alloca $ \(pcbResult :: Ptr B.ULONG) -> do
+    status <- B.c_BCryptGetProperty (sAlgHandler handler) propName valueBuf valueBufSize pcbResult 0
+    when (status < 0) $
+      fail "can't get property"
+    actualBufSize <- peek pcbResult
+    when (valueBufSize /= actualBufSize) $
+      fail "expected property value size doesn't match actual property value size"
+    return ()
+
+setAlgorithmProperty
+  :: (BCryptProperty p, PropertySet p)
+  => SymmetricAlgorithmHandler -> p -> PropertyValue p -> IO ()
+setAlgorithmProperty handler prop propVal =
+  withCWString (propertyName prop) $ \propName ->
+  marshalForward prop propVal $ \(buf, bufSize) -> do
+    status <- B.c_BCryptSetProperty (sAlgHandler handler) propName buf bufSize 0
+    when (status < 0) $
+      fail "can't set property"
+    return ()
