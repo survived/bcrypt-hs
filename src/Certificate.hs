d5c303fd1fc71da1f77702c26d18fb29d4082c8d
d86leader <d86leader@mail.com>
2019-12-18 16:48:45 +0700

    add code to get certificates from system store

---
+++ src/Certificate.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE LambdaCase #-}
+module Certificate
+( getCertByName
+, getPrivateKey
+, CertificateException
+) where
+
+import Control.Exception            (Exception, throw, bracket)
+import Control.Monad                (when)
+import Control.Monad.IO.Class       (MonadIO, liftIO)
+import Control.Monad.Trans.Class    (lift)
+import Control.Monad.Trans.Cont     (ContT (..), evalContT)
+import Control.Monad.Trans.Resource ( MonadResource, ReleaseKey, register
+                                    , allocate, runResourceT, release
+                                    )
+import Foreign.C.String             (peekCWString, withCString)
+import Foreign.Marshal.Alloc        (free, malloc, mallocBytes, allocaBytes, alloca)
+import Foreign.Ptr                  (Ptr, nullPtr)
+import Foreign.Storable             (peek, poke, Storable)
+
+import Bindings
+
+
+newtype CertificateException = CertificateException {what :: String}
+  deriving (Show)
+instance Exception CertificateException
+
+
+getCertByName :: MonadResource m => String -> m (ReleaseKey, Maybe PCCERT_CONTEXT)
+getCertByName name = allocate ( evalContT $ do
+    store      <- ContT allocateStore
+    retSizePtr <- ContT alloca
+    propIdPtr  <- ContT alloca
+    lift $ poke propIdPtr certFriendlyNamePropId
+    let startCert = nullPtr
+    lift $ iterateCertByName startCert store propIdPtr retSizePtr
+  )
+  freeMaybeCert
+  --
+  where
+    iterateCertByName prevCert store propIdPtr retSizePtr = do
+      cert <- c_CertFindCertificateInStore store allCertEncodings 0 certFindProperty propIdPtr prevCert
+      if cert == nullPtr
+      then return Nothing -- tried all certificates
+      else do
+        -- query for size
+        success <- c_CertGetCertificateContextProperty cert certFriendlyNamePropId nullPtr retSizePtr
+        size <- fmap fromIntegral . peek $ retSizePtr
+        allocaBytes size $ \propDataPtr -> do
+          _ <- c_CertGetCertificateContextProperty cert certFriendlyNamePropId propDataPtr retSizePtr
+          certName <- peekCWString propDataPtr
+          if certName == name
+          then return (Just cert)
+          else iterateCertByName cert store propIdPtr retSizePtr
+    --
+    freeMaybeCert Nothing     = pure ()
+    freeMaybeCert (Just cert) = c_CertFreeCertificateContext cert >> pure ()
+    --
+    allocateStore = bracket ( do
+        store <- withCString "MY" (c_CertOpenSystemStoreA nullPtr)
+        when (store == nullPtr) (throw $ CertificateException "failed to open certificate store")
+        pure store
+      )
+      (\store -> c_CertCloseStore store 0 >> pure ())
+
+
+getPrivateKey :: (MonadResource m, MonadIO m)
+              => PCCERT_CONTEXT -> m (ReleaseKey, NCRYPT_KEY_HANDLE)
+getPrivateKey cert = do
+  (keyHandle, releaseRequired) <- liftIO . evalContT $ do
+    legacyDwordPtr        <- ContT alloca
+    keyReleaseRequiredPtr <- ContT alloca
+    keyHandlePtr          <- ContT alloca
+    s <- lift $ c_CryptAcquireCertificatePrivateKey cert acquireOnlyNCryptFlag nullPtr keyHandlePtr
+                                                    legacyDwordPtr keyReleaseRequiredPtr
+    when (s == False) (throw $ CertificateException "Failed to acquire certifcate key")
+    keyHandle       <- lift $ peek keyHandlePtr
+    releaseRequired <- lift $ peek keyReleaseRequiredPtr
+    pure (keyHandle, releaseRequired)
+  releaseKey <- register $ if releaseRequired
+    then pure () -- TODO: real destoying
+    else pure ()
+  return (releaseKey, keyHandle)
---
+++ src/Certificate/Bindings.hsc
@@ -0,0 +1,122 @@
+module Bindings where
+
+import Data.Bits       ((.|.))
+import Data.Word       (Word32)
+import Foreign.C.Types (CChar)
+import Foreign.Ptr     (Ptr)
+
+#include <windows.h>
+#include <windows.h>
+#include <bcrypt.h>
+#include <ncrypt.h>
+
+type DWORD = Word32
+
+
+-- Store opening and closing
+
+data CERTSTORE = CERTSTORE -- msdn defines it as undefined type
+type HCERTSTORE = Ptr CERTSTORE
+
+-- HCERTSTORE CertOpenSystemStoreA(
+--   HCRYPTPROV_LEGACY hProv,
+--   LPCSTR            szSubsystemProtocol
+-- );
+foreign import ccall unsafe "CertOpenSystemStoreA"
+  c_CertOpenSystemStoreA
+    :: Ptr a -- nullptr_t actually
+    -> Ptr CChar -- should be "MY\0"
+    -> IO HCERTSTORE
+
+-- BOOL CertCloseStore(
+--   HCERTSTORE hCertStore,
+--   DWORD      dwFlags
+-- );
+foreign import ccall unsafe "CertCloseStore"
+  c_CertCloseStore
+    :: HCERTSTORE
+    -> DWORD
+    -> IO Bool
+
+
+-- Certificate opening and closing
+
+data CCERT_CONTEXT = CCERT_CONTEXT -- opaque
+type PCCERT_CONTEXT = Ptr CCERT_CONTEXT
+
+-- PCCERT_CONTEXT CertFindCertificateInStore(
+--   HCERTSTORE     hCertStore,
+--   DWORD          dwCertEncodingType,
+--   DWORD          dwFindFlags,
+--   DWORD          dwFindType,
+--   const void*    pvFindPara,
+--   PCCERT_CONTEXT pPrevCertContext
+-- );
+foreign import ccall unsafe "CertFindCertificateInStore"
+  c_CertFindCertificateInStore
+    :: HCERTSTORE
+    -> DWORD
+    -> DWORD
+    -> DWORD
+    -> Ptr a
+    -> PCCERT_CONTEXT
+    -> IO PCCERT_CONTEXT
+
+allCertEncodings :: DWORD
+allCertEncodings = #{const X509_ASN_ENCODING} .|. #{const PKCS_7_ASN_ENCODING}
+certFindProperty :: DWORD
+certFindProperty = #{const CERT_FIND_PROPERTY}
+certFriendlyNamePropId :: DWORD
+certFriendlyNamePropId = #{const CERT_FRIENDLY_NAME_PROP_ID}
+
+-- BOOL CertFreeCertificateContext(
+--   PCCERT_CONTEXT pCertContext
+-- );
+foreign import ccall unsafe "CertFreeCertificateContext"
+  c_CertFreeCertificateContext
+    :: PCCERT_CONTEXT
+    -> IO Bool
+
+
+-- Certificate properties stuff
+
+-- BOOL CertGetCertificateContextProperty(
+--   PCCERT_CONTEXT pCertContext,
+--   DWORD          dwPropId,
+--   void*          pvData,
+--   DWORD*         pcbData
+-- );
+foreign import ccall unsafe "CertGetCertificateContextProperty"
+  c_CertGetCertificateContextProperty
+    :: PCCERT_CONTEXT
+    -> DWORD
+    -> Ptr a
+    -> Ptr DWORD
+    -> IO Bool
+
+-- BOOL CryptAcquireCertificatePrivateKey(
+--   PCCERT_CONTEXT                  pCert,
+--   DWORD                           dwFlags,
+--   void*                           pvParameters,
+--   HCRYPTPROV_OR_NCRYPT_KEY_HANDLE *phCryptProvOrNCryptKey,
+--   DWORD*                          pdwKeySpec,
+--   BOOL*                           pfCallerFreeProvOrNCryptKey
+-- );
+foreign import ccall unsafe "CryptAcquireCertificatePrivateKey"
+  c_CryptAcquireCertificatePrivateKey
+    :: PCCERT_CONTEXT
+    -> DWORD
+    -> Ptr a
+    -> Ptr NCRYPT_KEY_HANDLE
+    -> Ptr DWORD
+    -> Ptr Bool
+    -> IO Bool
+
+acquireOnlyNCryptFlag :: DWORD
+acquireOnlyNCryptFlag = #{const CRYPT_ACQUIRE_ONLY_NCRYPT_KEY_FLAG}
+
+
+-- NCrypt keys
+
+data NCRYPT_KEY = NCRYPT_KEY
+type NCRYPT_KEY_HANDLE = Ptr NCRYPT_KEY
