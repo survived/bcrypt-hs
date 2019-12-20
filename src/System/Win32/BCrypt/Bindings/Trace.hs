8cce77542897adda3baad4bcafee87ebe7d1fe5a
Denis Varlakov <dsurv@yandex.ru>
2019-12-20 22:48:31 +0700

    Also trace returning status

--- src/System/Win32/BCrypt/Bindings/Trace.hs
+++ src/System/Win32/BCrypt/Bindings/Trace.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 module System.Win32.BCrypt.Bindings.Trace
   ( module System.Win32.BCrypt.Bindings.Trace
   , module Reexports
@@ -8,38 +9,41 @@ import System.Win32.BCrypt.Bindings.Externals as Reexports
 import qualified System.Win32.BCrypt.Bindings.Externals as B
 
 c_BCryptOpenAlgorithmProvider =
-  hookAfter B.c_BCryptOpenAlgorithmProvider $
-    putStrLn "BCryptOpenAlgorithmProvider is called"
+  hookAfter B.c_BCryptOpenAlgorithmProvider $ \status ->
+    putStrLn $ "BCryptOpenAlgorithmProvider returned " ++ show status
 c_BCryptCloseAlgorithmProvider =
-  hookAfter B.c_BCryptCloseAlgorithmProvider $
-    putStrLn "BCryptCloseAlgorithmProvider is called"
+  hookAfter B.c_BCryptCloseAlgorithmProvider $ \status ->
+    putStrLn $ "BCryptCloseAlgorithmProvider returned " ++ show status
 c_BCryptGetProperty =
-  hookAfter B.c_BCryptGetProperty $
-    putStrLn "BCryptGetProperty is called"
+  hookAfter B.c_BCryptGetProperty $ \status ->
+    putStrLn $ "BCryptGetProperty returned " ++ show status
 c_BCryptSetProperty =
-  hookAfter B.c_BCryptSetProperty $
-    putStrLn "BCryptSetProperty is called"
+  hookAfter B.c_BCryptSetProperty $ \status ->
+    putStrLn $ "BCryptSetProperty returned " ++ show status
 c_BCryptGenerateSymmetricKey =
-  hookAfter B.c_BCryptGenerateSymmetricKey $
-    putStrLn "BCryptGenerateSymmetricKey is called"
+  hookAfter B.c_BCryptGenerateSymmetricKey $ \status ->
+    putStrLn $ "BCryptGenerateSymmetricKey returned " ++ show status
 c_BCryptDestroyKey =
-  hookAfter B.c_BCryptDestroyKey $
-    putStrLn "BCryptDestroyKey is called"
+  hookAfter B.c_BCryptDestroyKey $ \status ->
+    putStrLn $ "BCryptDestroyKey returned " ++ show status
 c_BCryptEncrypt =
-  hookAfter B.c_BCryptEncrypt $
-    putStrLn "BCryptEncrypt is called"
+  hookAfter B.c_BCryptEncrypt $ \status ->
+    putStrLn $ "BCryptEncrypt returned " ++ show status
 c_BCryptDecrypt =
-  hookAfter B.c_BCryptDecrypt $
-    putStrLn "BCryptDecrypt is called"
+  hookAfter B.c_BCryptDecrypt $ \status ->
+    putStrLn $ "BCryptDecrypt returned " ++ show status
 
 class Hookable a where
-  hookAfter :: a -> IO () -> a
+  type Result a
+  hookAfter :: a -> (Result a -> IO ()) -> a
 
 instance Hookable (IO a) where
+  type Result (IO a) = a
   hookAfter act hook = do
     result <- act
-    hook
+    hook result
     return result
 
 instance Hookable b => Hookable (a -> b) where
+  type Result (a -> b) = Result b
   hookAfter f hook a = hookAfter (f a) hook
--- src/System/Win32/Certificate/Bindings/Trace.hs
+++ src/System/Win32/Certificate/Bindings/Trace.hs
@@ -11,29 +11,29 @@ import System.Win32.Certificate.Bindings.Externals as Reexports
 import qualified System.Win32.Certificate.Bindings.Externals as B
 
 c_CertOpenSystemStoreA =
-  hookAfter B.c_CertOpenSystemStoreA $
-    putStrLn "c_CertOpenSystemStoreA is called"
+  hookAfter B.c_CertOpenSystemStoreA $ \status ->
+    putStrLn $ "c_CertOpenSystemStoreA returned " ++ show status
 c_CertCloseStore =
-  hookAfter B.c_CertCloseStore $
-    putStrLn "c_CertCloseStore is called"
+  hookAfter B.c_CertCloseStore $ \status ->
+    putStrLn $ "c_CertCloseStore returned " ++ show status
 c_CertFindCertificateInStore =
-  hookAfter B.c_CertFindCertificateInStore $
-    putStrLn "c_CertFindCertificateInStore is called"
+  hookAfter B.c_CertFindCertificateInStore $ \status ->
+    putStrLn $ "c_CertFindCertificateInStore returned " ++ show status
 c_CertFreeCertificateContext =
-  hookAfter B.c_CertFreeCertificateContext $
-    putStrLn "c_CertFreeCertificateContext is called"
+  hookAfter B.c_CertFreeCertificateContext $ \status ->
+    putStrLn $ "c_CertFreeCertificateContext returned " ++ show status
 c_CertGetCertificateContextProperty =
-  hookAfter B.c_CertGetCertificateContextProperty $
-    putStrLn "c_CertGetCertificateContextProperty is called"
+  hookAfter B.c_CertGetCertificateContextProperty $ \status ->
+    putStrLn $ "c_CertGetCertificateContextProperty returned " ++ show status
 c_CryptAcquireCertificatePrivateKey =
-  hookAfter B.c_CryptAcquireCertificatePrivateKey $
-    putStrLn "c_CryptAcquireCertificatePrivateKey is called"
+  hookAfter B.c_CryptAcquireCertificatePrivateKey $ \status ->
+    putStrLn $ "c_CryptAcquireCertificatePrivateKey returned " ++ show status
 c_NCryptFreeObject =
-  hookAfter B.c_NCryptFreeObject $
-    putStrLn "c_NCryptFreeObject is called"
+  hookAfter B.c_NCryptFreeObject $ \status ->
+    putStrLn $ "c_NCryptFreeObject returned " ++ show status
 c_NCryptGetProperty =
-  hookAfter B.c_NCryptGetProperty $
-    putStrLn "c_NCryptGetProperty is called"
+  hookAfter B.c_NCryptGetProperty $ \status ->
+    putStrLn $ "c_NCryptGetProperty returned " ++ show status
 c_NCryptEncrypt =
-  hookAfter B.c_NCryptEncrypt $
-    putStrLn "c_NCryptEncrypt is called"
+  hookAfter B.c_NCryptEncrypt $ \status ->
+    putStrLn $ "c_NCryptEncrypt returned " ++ show status
